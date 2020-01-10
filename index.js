3fccc3e6002ffab2796431259bbd414900f7eeff
Мохирбек Рузметов <mohirbek.omad777@gmail.com>
2020-01-10 01:36:17 -0300

    TelegramBot Studentlar uchun!

---
+++ index.js
@@ -0,0 +1,18 @@
+const Telegram = require('node-telegram-bot-api')
+const TOKEN = '1002372864:AAHl6rFV5gTauQQd085DlkkC7wQOImP3MSQ'
+
+console.log('Bot has been started... ')
+
+const bot = new Telegram(TOKEN, {
+    polling: {
+        interval: 300,
+        autoStart: true,
+        params: {
+            timeout: 10
+        }
+    }
+})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, 'I am alive!')
+})
